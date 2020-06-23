14dbc6d858f8640f8da049beab060a932e203685
nicol <nicol@DESKTOP-E1N4B9K>
2020-06-23 14:48:56 -0400

    Hand Class was changed to abstract class and has new features like showhand and hidehand

--- src/gofishdeliverable1/Hand.java
+++ src/gofishdeliverable1/Hand.java
@@ -12,62 +12,74 @@ import java.util.ArrayList;
  * This class <enter description>
  * @author Nick De Luca
  */
-public class Hand {
+public abstract class Hand{
 
     private ArrayList<Card> hand = new ArrayList();
-    private int handSize;
+    private int handSizeLimit;
 
     public Hand() {
     }
     
-    public Hand(int handSize) {
-        setHandSize(handSize);
+    public Hand(int handSizeLimit) {
+        setHandSizeLimit(handSizeLimit);
     }
 
     public ArrayList<Card> getHand() {
         return hand;
     }
+     public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
     
-    public void setHandSize(int handSize){
-        if(handSize>0){
-            this.handSize = handSize;
+    public ArrayList<Card> sortHand(){
+        Card lowest = hand.get(0);
+        for (int i = 0; i < hand.size(); i++) {
+            for (int j = 1; j < hand.size(); j++) {
+                if(hand.get(j).getValue() < hand.get(j-1).getValue()){
+                    lowest = hand.get(j);
+                    hand.remove(j);
+                    hand.add(0,lowest);
+                }
+            }
+        }
+        return hand;
+    }
+    
+    public void setHandSizeLimit(int handSizeLimit){
+        if(handSizeLimit>0){
+            this.handSizeLimit = handSizeLimit;
         }else{
             throw new IllegalArgumentException("Hand Size must be positive");
         }
     }
     
-    public int getHandSize(){
-        return handSize;
+    public int getHandSizeLimit(){
+        return handSizeLimit;
     }
-    //returns true or false if remove was successful(i.e. if the player has the card or not)
-    public boolean removeFromHand(Card card){
-        for (Card c : hand) {
-           
-            if(c.getSuit().equalsIgnoreCase(card.getSuit()) && c.getValue()==card.getValue()){
-                hand.remove(c);
-                return true;
-            }
-        }
-        return false;
-    }
+    
+    //returns true or false if remove was successful(j.e. if the player has the card or not)
+   
+    
     //adds a card to the player's hand instance var object
     //returns true if you can add a card
     //returns false if the hand is full
-    //u can change the return to void if needed, i just thought it might be useful.
+    //u can change the return to void if needed, j just thought it might be useful.
     public boolean addToHand(Card card){
-        if(hand.size() <= handSize){
+        if(hand.size() < handSizeLimit){
             hand.add(card);
         }else{
             return false;
         }
         return true;
+    }
+    
+     public boolean removeFromHand(Card card) {
         
-    }
-
-    @Override
-    public String toString() {
-        return "Hand: " + "hand=" + hand;
+        return hand.remove(card);
     }
     
+    public abstract String showHand();
     
+    public abstract boolean checkHand(Card card);
+ 
 }
